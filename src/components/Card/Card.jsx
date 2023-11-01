464decc409e3403322d4b768f92432bb2a84c796
kevin Pajaniandy <122272608+KevinPajaniandy@users.noreply.github.com>
2023-11-01 18:43:45 +0200

    Finalisation paga d'accueil

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ export default function Card({ id, cover, title }) {
         <article className='card-lease'>
             {/* create a link that naviagate to the 'Lodging' page with the corresponding 'id' */}
             <Link to={`/lodging/${id}`}>
-                <div className='card-lease_ovelay'></div>
+                <div className='card-lease_overlay'></div>
                 <img className="card-lease_img" src={cover} alt={title} />
                 <h2 className='card-lease_title'>{title}</h2>
             </Link>
